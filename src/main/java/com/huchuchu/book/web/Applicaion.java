5189d0a674156e6b0178d70bdab97f52eee46290
huchuchu <huchuchu.yoo@gmail.com>
2021-02-20 21:38:58 +0900

    new note

---
+++ src/main/java/com/huchuchu/book/web/Applicaion.java
@@ -0,0 +1,14 @@
+package com.huchuchu.book.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Applicaion {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Applicaion.class, args);
+    }
+
+
+}
